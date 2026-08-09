db5ccde84f85b45cd3fced61d293dc779c46a8c7
asdisnotrandom <demirelahme123@gmail.com>
2026-08-09 04:30:36 +0300

    oturuyor

--- py7/ex2/battle.py
+++ py7/ex2/battle.py
@@ -10,33 +10,18 @@ class ValidError(Exception):
 
 class BattleStrategy(abc.ABC):
     def __init__(self, creatur: Creature) -> None:
-        self.creatur = creatur
-
-    @abc.abstractmethod
-    def is_valid(self) -> bool:
         if not isinstance(self.creatur, Creature):
             raise ValidError(f"{self.creatur} is not a Creature!")
             return False
-        if self.strat == "Normal":
-            return True
-        if self.strat == "Defensive":
-            if not isinstance(self.creatur, HealCapability):
-                raise ValidError(f"Battle Error: {self.creatur} needs to be"
-                                 f"subclass of HealingCapability"
-                                 f"for Defensive strategy!")
-                return False
-            return True
-        if self.strat == "Aggressive":
-            if not isinstance(self.creatur, TransformCapability):
-                raise ValidError(f"Battle Error: {self.creatur} needs to be"
-                                 f"subclass of TransformCapability"
-                                 f"for Aggressive strategy!")
-                return False
-            return True
         else:
-            raise ValidError(f"Battle Error: {self.strat} is not"
-                             f"valid strategy for battle!")
-            return False
+            self.creatur = creatur
+
+    @abc.abstractmethod
+    def is_valid(self) -> bool:
+        pass
+
+    def act(self) -> None:
+        pass
 
 
 class NormalStrategy(BattleStrategy):
@@ -44,6 +29,39 @@ class NormalStrategy(BattleStrategy):
         super().__init__(creatur)
 
     def is_valid(self) -> bool:
-        if not isinstance(self.creatur, Creature):
-            raise ValidError(f"{self.creatur} is not a Creature!")
+        return True
+
+    def act(self) -> None:
+        pass
+
+
+class DefensiveStrategy(BattleStrategy):
+    def __init__(self, creatur: Creature) -> None:
+        super().__init__(creatur)
+
+    def is_valid(self) -> bool:
+        if not isinstance(self.creatur, HealCapability):
+            raise ValidError(f"Battle Error: {self.creatur} needs to be"
+                                 f"subclass of HealingCapability"
+                                 f"for Defensive strategy!")
             return False
+        return True
+
+    def act(self) -> None:
+        pass
+
+
+class AggressiveStrategy(BattleStrategy):
+    def __init__(self, creatur: Creature) -> None:
+        super().__init__(creatur)
+
+    def is_valid(self) -> bool:
+        if not isinstance(self.creatur, TransformCapability):
+            raise ValidError(f"Battle Error: {self.creatur} needs to be"
+                                f"subclass of TransformCapability"
+                                f"for Aggressive strategy!")
+            return False
+        return True
+
+    def act(self) -> None:
+        pass 
